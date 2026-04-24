3c18eb93d3f7dc1f8931f2a03cf1fc10073e09fa
Chris Havlin <chavlin@illinois.edu>
2026-04-24 16:56:45 -0500

    add runtime config with nlohmann json

--- main.cpp
+++ main.cpp
@@ -9,11 +9,16 @@
 #include <chrono>
 using namespace std::literals::chrono_literals;
 
+#include <nlohmann/json.hpp>
+using json = nlohmann::json;
+
 #include <csignal>
 #include <functional>
 #include <filesystem>
 
-void launchFoxglove() {
+using namespace std;
+
+void launchFoxglove(std::string config_filename) {
     foxglove::setLogLevel(foxglove::LogLevel::Debug);
 
     static std::function<void()> sigint_handler;
@@ -35,17 +40,53 @@ void launchFoxglove() {
 
     auto fgInterface = FoxgloveInterface{"viper.mcap"};
     std::this_thread::sleep_for(1000ms);
+   
+    float offset_x = 0.150;
+    float offset_y = 0.0;
+    float offset_z = 0.0;
+    float min_contact_force = 0.35;
+    std::string &&pressure_port = "/dev/ttyACM0";
 
-    Viper viper{&fgInterface, 10, 100};
-    viper.setOffset(0.150, 0, 0); // slim: (0.150, 0, 0); YOP: (0.157, 0, 0)
-
-    SerialForce serialForce{fgInterface};
-    serialForce.setContactCallback(
-        [](mdx::RawForce &force) {
-            return force.f4 > 0.35;
+    if (std::filesystem::exists(config_filename))
+    {
+        cout << "Parsing config file: " << config_filename << endl;
+        std::ifstream f(config_filename);
+        json settings = json::parse(f);
+        if (settings.contains("offset_x"))
+        {
+            offset_x = settings["offset_x"];
         }
-    );
-    serialForce.init("/dev/ttyACM0");
+        if (settings.contains("offset_y"))
+        {
+            offset_y = settings["offset_y"];
+        }
+        if (settings.contains("offset_z"))
+        {
+            offset_z = settings["offset_z"];
+        }
+        if (settings.contains("minimum_contact_force"))
+        {
+            // min_contact_force = settings["minimum_contact_force"];
+            cout << "    Warning: minimum_contact_force not used at present\n";
+        }
+        if (settings.contains("pressure_device_port"))
+        {
+            pressure_port = settings["pressure_device_port"];
+        }
+
+        cout << "    Done parsing.\n";
+    }
+    else
+    {
+        cout << "No config file found. using default values\n";
+    }
+
+    cout << "\nRuntime settings:\n";
+    cout << "    offset_x:" << offset_x << endl;
+    cout << "    offset_y:" << offset_y << endl;
+    cout << "    offset_z:" << offset_z << endl;
+    // cout << "    minimum_contact_force:" << min_contact_force << endl;
+    cout << "    pressure_device_port:" << pressure_port << endl;
 
     std::atomic_bool done = false;
     sigint_handler = [&]
@@ -76,8 +117,13 @@ void launchFoxglove() {
 }
 
 
-int main() {
-    launchFoxglove();
+int main(int argc, char** argv) {
+
+    std::string config_filename = "viper-config.json";
+    if (argc > 1){
+        config_filename = argv[1];
+    }
+    launchFoxglove(config_filename);
 
     return 0;
 }
